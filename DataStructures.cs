b559319b8a2dae51b12278f253f59733e59b15b0
Danialmohebbi <daniyalmohebbi@yahoo.com>
2024-05-05 22:53:15 +0200

    Update Project

--- DataStructures.cs
+++ DataStructures.cs
@@ -1,5 +1,5 @@
-using System;
-using System.ComponentModel;
+using utilities;
+
 namespace DataStructures
 {
     class Node{
@@ -35,29 +35,214 @@ class Stack{
         head = head.next;
         return popped_value;
     }
-
 }
 
+/// <summary>
+/// this class represents the main 2D array which will be used for reading the instructions.
+/// </summary>
 class Field{
-    public void Instruction_field_printer(char[,] field)
-    {
-        for (int i = 0; i < field.GetLength(0); i++)
-        {
-            for (int j = 0; j < field.GetLength(1); j++)
-            {
-                Console.Write("[" + field[i, j] + "]");
-            }
-            Console.WriteLine();
-        }
+    public char[,] instruction_field = new char[25,80];    
+    int[] x_len = new int[25];
+    int[] y_len = new int[80];
+    int pointer_x;
+    int pointer_y;
+    public int direction_x;
+    public int direction_y;
+    (int, int) RIGHT = (1, 0);
+    (int, int) LEFT = (-1, 0);
+    (int, int) DOWN = (0, 1);
+    (int, int) UP = (0, -1);
+    (int,int)[] directions = {(1,0), (-1,0), (0,1), (0,-1)};
+    
+    Utilities utilities = new();
+    Stack stack = new Stack();
+
+    Random r = new();
+
+
+    public Field(){
+        (direction_x, direction_y) = RIGHT;
+        (pointer_x, pointer_y) = (0,0);
     }
 
-    public void Field_initialization(char[,] field, int line, string input)
+    /// <summary>
+    /// parse a line into a row in the 2D array.
+    /// </summary>
+    /// <param name="line">2D array row number</param>
+    /// <param name="input">the string to be parsed into instructions</param>
+    /// <returns>if the line contains more than 25 characters</returns>
+    public void Parse(int line, string input)
     {
+        if (input.Length > 80){
+            throw new ArgumentException("All lines should contain at most 80 charachters!");
+        }
         for (int i = 0; i < input.Length; i++)
         {
-            field[line, i] = input[i];
+            instruction_field[line, i] = input[i];
 
         }
     }
-}
+    /// <summary>
+    /// read the inputed file for instructions
+    /// </summary>
+    /// <param name="fileName">file name</param>
+    /// <returns>if number of lines more than 25 or the file doesn't exist throws an exception</returns>
+    public void processFile(string fileName){
+
+        List<string> readInstructions = new();
+        
+        if (!File.Exists(fileName)){
+            throw new Exception(fileName + " does not exist!");
+        }
+
+        using StreamReader reader = new(fileName);
+        while (reader.ReadLine() is string line){
+            readInstructions.Add(line);
+
+        }
+        if (readInstructions.Count > 25){
+            throw new Exception("The file contains more than 25 lines!");
+        }
+
+        for (int i = 0; i < readInstructions.Count; i++){
+            Parse(i, readInstructions[i]);
+        }
+
+        
+    }
+    /// <summary>
+    /// get the next token/instructions to process
+    /// </summary>
+    /// <returns>char</returns>
+    public char nextToken(){
+        var token = instruction_field[pointer_y,pointer_x]; 
+        return token;
+    }
+    /// <summary>
+    /// change the direction that the pointer will be moving 
+    /// </summary>
+    /// <param name="op">the instruction to be evaluated</param>
+    /// <returns>null</returns>
+    public void changeDirection(char op){
+        switch (op)
+        {
+            case '>': (direction_x, direction_y) = RIGHT;break;            
+            case '<': (direction_x, direction_y) = LEFT;break;
+            case 'v': (direction_x, direction_y) = DOWN;break;
+            case '^': (direction_x, direction_y) = UP;break;
+            case '?': 
+            (direction_x, direction_y) = directions[r.Next(0,4)];break;
+            default:break;
+        }
+    }
+    /// <summary>
+    /// evaluate if a direction change is needed
+    /// </summary>
+    /// <param name="op">the instruction to be evaluated</param>
+    /// <param name="stack">the stack to be used for evaluation</param>
+    /// <returns>null</returns>
+    public void evaluateDirection(char op, ref Stack stack){
+        int value;
+        switch (op){
+            case '_': 
+            value = stack.pop();
+            if (value == 0){
+                (direction_x, direction_y) = RIGHT;
+            }
+            else {
+                (direction_x,direction_y) = LEFT;
+            }
+            break;
+            case '|':
+            value = stack.pop();
+            if (value == 0){
+                (direction_x, direction_y) = DOWN;
+            }else {
+                (direction_x, direction_y) = UP;
+            }
+            break;
+            default: break;
+        }
+    }
+    /// <summary>
+    /// update the pointers accordingly to get the next instruction
+    /// </summary>
+    /// <param name="x">choose how to update the x of the pointer</param>
+    /// <param name="y">choose how to update the y of the pointer</param>
+    /// <returns>null</returns>
+    public void updatePointers(int x = 0, int y = 0){
+        pointer_x  = (pointer_x + x + 80) % 80;
+        pointer_y = (pointer_y + y + 25) % 25;
+        }
+    /// <summary>
+    /// contains the main loop and process the instructions in the field 
+    /// </summary>
+    /// <returns>null</returns>
+    public void eval(){
+        bool running = true;
+
+        while (running){
+            char currentInstruction = nextToken();
+            if (int.TryParse(Convert.ToString(currentInstruction), out int value))
+            {
+                stack.push(value);
+            }
+            else if ("+-/*%!`".Contains(currentInstruction)){
+                utilities.evaluateArithmatics(currentInstruction, ref stack);
+            }
+            else if (".,".Contains(currentInstruction)){
+                utilities.output(currentInstruction, ref stack);
+            }
+            else if (">v^?<".Contains(currentInstruction)){
+                changeDirection(currentInstruction);
+            }
+            else if(currentInstruction == '#'){
+                updatePointers(direction_x, direction_y);
+            }
+            else if ("|_".Contains(currentInstruction)){
+                evaluateDirection(currentInstruction, ref stack);
+            }
+            else if ("&~".Contains(currentInstruction)){
+                utilities.evaluateInput(currentInstruction, ref stack, ref running);
+            }
+            else if (currentInstruction == '"'){
+                updatePointers(direction_x,direction_y);
+                while (true){
+                    char newInstruction = nextToken();
+                    if (newInstruction == '"'){
+                        updatePointers();
+                        break;
+                    }
+                    stack.push(newInstruction);
+                    updatePointers(direction_x, direction_y);
+                }
+            }
+            else if (":\\$".Contains(currentInstruction)){
+                utilities.evaluateStack(currentInstruction, ref stack);
+            }
+            else if (currentInstruction == 'g'){
+                int y = stack.pop();
+                int x = stack.pop();
+                if (0 <= y && y <= 25 && x <= 80 && 0 <= x){
+                    char getValue = instruction_field[y,x];
+                    stack.push(getValue);
+                }else{
+                    stack.push(0);
+                }
+            }else if (currentInstruction == 'p'){
+                int y = stack.pop();
+                int x = stack.pop();
+                int val = stack.pop();
+                instruction_field[y,x] = (char) val;
+            }
+            else if (currentInstruction == '@'){
+                break;
+            }
+
+            updatePointers(direction_x, direction_y);
+
+        }
+    }
+    }
+
 }
---
+++ Utilities.cs
@@ -0,0 +1,147 @@
+using DataStructures;
+namespace utilities
+{
+    class Utilities{
+    /// <summary>
+    /// this is a helper method to evaluate the following arithmatic opreations:
+    /// +,-,/,*,%,!.
+    /// </summary>
+    /// <param name="op">the instruction to be evaluated</param>
+    /// <param name="stack">the stack to be used for evaluation</param>
+    /// <returns>null</returns>
+public void evaluateArithmatics(char op, ref Stack stack){
+
+    int value_1 = 0;
+    int value_2 = 0;
+    switch(op){
+        case '+': stack.push(stack.pop() + stack.pop()); break;
+        case '-': 
+        value_1 = stack.pop();
+        value_2 = stack.pop();
+        stack.push(value_2 - value_1); break;
+        case '*': stack.push(stack.pop() * stack.pop()); break;
+        case '/': 
+        value_1 = stack.pop();
+        value_2 = stack.pop();
+        if (value_1 == 0){
+            stack.push(0);
+        }else{
+            stack.push(value_2 / value_1);
+        }
+        break;
+        case '%':
+        value_1 = stack.pop();
+        value_2 = stack.pop();
+        stack.push(value_1 % value_2); break;
+        case '!':
+        value_1 = stack.pop();
+        if (value_1 == 0){
+            stack.push(1);
+        }else{
+            stack.push(0);
+        }
+        break;
+        case '`':
+        int a = stack.pop();
+        int b = stack.pop();
+        stack.push(b > a ? 1 : 0);
+        break;
+        default:
+        break;
+    }
+}
+
+/// <summary>
+/// this is a helper to process the command line arguments
+/// </summary>
+/// <param name="args">command line arguments</param>
+/// <returns>String Filename</returns>
+public string processArguments(string[] args){
+    if (args.Length != 1){
+        Console.WriteLine("dotnet run FileName");
+        throw new("Please enter a filename!");
+    }
+    return args[0];
+}
+
+/// <summary>
+/// this is a helper method to output an integer or char which has the following commands:
+/// . output top stack value as an int
+/// , output top stack value as an char
+/// </summary>
+/// <param name="op">the instruction to be evaluated</param>
+/// <param name="stack">the stack to be used for evaluation</param>
+/// <returns>null</returns>
+public void output(char op, ref Stack stack){
+    switch (op)
+    {
+        case '.': Console.Write((int)stack.pop() + " "); break;
+        case ',': Console.Write((char) stack.pop()); break;
+        default:break;
+    }
+}
+
+
+/// <summary>
+/// this is a helper method to evaluate an input, which has the following commands:
+/// & for integer input
+/// ~ for char input
+/// </summary>
+/// <param name="op">the instruction to be evaluated.</param>
+/// <param name="stack">the stack to be used for evaluation.</param>
+/// <param name="running">to stop the program if needed.</param>
+/// <returns>null</returns>
+public void evaluateInput(char op, ref Stack stack, ref bool running){
+    switch(op)
+    {
+        case '&':
+        string? input_integer = Console.ReadLine();
+        if (input_integer is null || input_integer == ""){
+            running = false;
+            break;
+        }
+        stack.push(int.Parse(input_integer));
+        break;
+        case '~': 
+        string? str = Console.ReadLine();
+        if (str == "" || str is  null){
+            running = false;
+            break;
+        }
+        stack.push(str[0]); 
+        break;
+
+    }
+}
+
+/// <summary>
+/// this is a helper method that does the following elementary opreations on the stack:
+/// $ pops an element
+/// : duplicates the top element on the stack
+/// \ swaps the top two elements on the stack
+/// </summary>
+/// <param name="op">the instruction to be evaluated.</param>
+/// <param name="stack">the stack to be used for evaluation.</param>
+/// <returns>null</returns>
+public void evaluateStack(char op, ref Stack stack){
+    switch (op)
+    {
+        case '$': stack.pop(); break;
+        case '\\':
+        int value_1 = stack.pop();
+        int value_2 = stack.pop();
+        stack.push(value_1);
+        stack.push(value_2);
+        break;
+        case ':':
+        int value_3 = stack.pop();
+        stack.push(value_3);
+        stack.push(value_3); 
+        break;
+        default:
+        break;
+    }
+}
+
+}
+}
